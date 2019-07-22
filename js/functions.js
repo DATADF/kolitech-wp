f0391994c01518063658e9b9ba8518a8e81774e6
DATADF <klaytonfantin@outlook.com>
2019-07-22 15:23:11 -0300

    Partials / Scroll / Nav Active

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,40 @@
-$('.navbar-nav a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href'),
-        targetOffset = $(id).offset().top;
+// Rolagem quando clica no menu
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
 
-    $('html, body').animate({
-        scrollTop: targetOffset - 100
-    }, 500);
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('#navbar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbarr ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
